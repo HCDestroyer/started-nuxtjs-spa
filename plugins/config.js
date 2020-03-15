ef00eda21c04b28c3385cd2608f3b9250dac5a19
Angel Calán <angelcalan5@gmail.com>
2020-03-15 09:51:06 -0600

    sounds added

--- plugins/config.js
+++ plugins/config.js
@@ -27,6 +27,12 @@ Vue.mixin({
                 info: false,
                 save: false,
                 search: false,
+                one: false,
+                two: false,
+                three: false,
+                four: false,
+                five: false,
+                six: false,
             },
             overlay: {
                 text: '',
@@ -47,6 +53,35 @@ Vue.mixin({
     },
     methods:
     {
+        hiddenProps(obj={}, props =[])
+        {
+            let hiddenProp = obj;
+            Object.keys(hiddenProp).map((prop) =>
+            {
+                props.map((hidden) => {
+                    if (hidden === prop) 
+                    {
+                        delete hiddenProp[hidden];
+                    }
+                });
+            });
+            return hiddenProp;
+        },
+        onlyProps(obj = {}, props = []) {
+            let onlyProp = obj;
+            let newOb = {};
+            if(props.length>0)
+            {
+                Object.keys(onlyProp).map((prop) =>
+                {
+                    if (props.indexOf(prop) >= 0)
+                    {
+                        newOb[prop] = onlyProp[prop];
+                    }
+                });
+            }
+            return newOb;
+        },
         authCheckPermission(permission)
         {
             console.log(this.$store.$auth.user.access.includes(permission));
@@ -63,19 +98,19 @@ Vue.mixin({
             this.display_info.list = true;
             this.display_info.card = false;
         },
-        getQR(text = "BINPAY") {
+        getQR(text = "NAMEAPP") {
             var svg_string = QrImage.imageSync(text);
             var base64Data = btoa(String.fromCharCode.apply(null, svg_string));
             var img = 'data:image/png;base64,' + base64Data;
             return img;
         },
-        customerQR(text = "BINPAY") {
+        customerQR(text = "NAMEAPP") {
             var svg_string = QrImage.imageSync(this.app_url + '/customer/' + text);
             var base64Data = btoa(String.fromCharCode.apply(null, svg_string));
             var img = 'data:image/png;base64,' + base64Data;
             return img;
         },
-        appQR(text = "BINPAY") {
+        appQR(text = "NAMEAPP") {
             var svg_string = QrImage.imageSync(text);
             var base64Data = btoa(String.fromCharCode.apply(null, svg_string));
             var img = 'data:image/png;base64,' + base64Data;
@@ -87,8 +122,29 @@ Vue.mixin({
         trans(text) {
             var word = new String(text).trim();
             return (this.$store.getters['translate/trans'][word]) ? this.$store.getters['translate/trans'][word] : 'lang:' + text;
-        },
-        pushMessage(text, type = 'black', time = 5000, buttons = []) {
+        },      
+        pushMessage(text, type = 'black', time = 5000, buttons = []) 
+        {
+            var audio = new Audio();
+            switch (type) 
+            {
+                case 'success':
+                    audio.src = this.app_url + '/sounds/success.ogg';
+                    break;
+                case 'warning':
+                    audio.src = this.app_url + '/sounds/warning.ogg';
+                    break;
+                case 'error':
+                    audio.src = this.app_url + '/sounds/error.ogg';
+                    break;
+                case 'black':
+                    audio.src = this.app_url + '/sounds/popup.ogg';
+                    break;
+                default:
+                    audio.src = this.app_url + '/sounds/notifications.ogg';
+                    break;
+            }
+            audio.play();
             this.snackbar = {
                 type: type,
                 time: time,
@@ -108,6 +164,54 @@ Vue.mixin({
             num = parseInt(num);
             num = (isNaN(num) ? 0 : num);
             return num;
-        }
+        },
+        getFormatParams(obj, paramsFilters) 
+        {
+            let new_obj = {};
+            paramsFilters.map((header) => 
+            {
+                if (header.date) 
+                {
+                    Object.defineProperty(new_obj, header.text,
+                    {
+                        value: this.getDate(obj[header.value]),
+                        writable: true,
+                        enumerable: true,
+                        configurable: true
+                    });
+                }
+                else if (header.float) 
+                {
+                    Object.defineProperty(new_obj, header.text,
+                    {
+                        value: Number.parseFloat(parseFloat(obj[header.value]).toFixed(2)),
+                        writable: true,
+                        enumerable: true,
+                        configurable: true
+                    });
+                }
+                else if (header.integer) 
+                {
+                    Object.defineProperty(new_obj, header.text,
+                        {
+                            value: Number.parseInt(obj[header.value]),
+                            writable: true,
+                            enumerable: true,
+                            configurable: true
+                        });
+                }
+                else 
+                {
+                    Object.defineProperty(new_obj, header.text,
+                    {
+                        value: obj[header.value],
+                        writable: true,
+                        enumerable: true,
+                        configurable: true
+                    });
+                }
+            });
+            return new_obj;
+        },
     }
 })
